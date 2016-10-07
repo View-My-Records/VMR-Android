a6c77efd4c8b7939e287d86bd12d50428a52c198
AbhijitParate <abhijitpparate@gmail.com>
2016-10-08 03:37:06 +0530

    Fixed version code and version timestamp

--- app/src/main/java/com/vmr/login/LoginActivity.java
+++ app/src/main/java/com/vmr/login/LoginActivity.java
@@ -84,7 +84,7 @@ public class LoginActivity extends AppCompatActivity
         } else if (id == R.id.action_about) {
             new AlertDialog.Builder(this)
                     .setTitle("About")
-                    .setMessage("Version Code: " + BuildConfig.VERSION_CODE + "\n\n" + "Version TimeStamp: " + BuildConfig.VERSION_NAME)
+                    .setMessage("Version Code: \n\t" + BuildConfig.VERSION_CODE + "\n\n" + "Version TimeStamp: \n\t" + BuildConfig.VERSION_NAME)
                     .show();
         }
 
